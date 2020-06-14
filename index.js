9d71b40781779290d3688de7da1e309357938708
Try Ajitiono <ballinst@gmail.com>
2020-06-14 13:49:21 +0700

    add sample of referencing variables with JSDoc comments
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- index.js
+++ index.js
@@ -11,3 +11,11 @@ const y = 2;
 const z = 3;
 
 // Note that the output comment for `y` and `z` are the same, despite we use line break in the comment.
+
+// If we later use `y` and `z`, when we hover over them, we can see
+// the JSDoc comments. This enables us to infer what kind of variable are they
+// without needing to scroll up (assuming that this file has ~500 lines or so).
+
+//
+//
+const total = y + z;
